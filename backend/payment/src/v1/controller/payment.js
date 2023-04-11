0fbdf56270b03222b356ad20c2a2262d342664c9
Supun Kariyawasam <bhasura2105@gmail.com>
2023-04-11 16:04:06 +0530

    changes to the server, built get and delete payment endpoints

--- backend/payment/src/v1/controller/payment.js
+++ backend/payment/src/v1/controller/payment.js
@@ -36,3 +36,28 @@ exports.getPayments = async (req, res) => {
     }
 
 };
+
+exports.getPayment = async (req, res) => {
+
+    try{
+        let paymentId = req.params.id;
+        const payment = await Payment.findById(paymentId);
+        return res.status(200).json({ payment });
+    } catch (error){
+        return res.status(400).json({ error });
+    }
+    
+};
+
+exports.deletePayment = async (req, res) => {
+
+    try{
+        let paymentId = req.params.id;
+        await Payment.findByIdAndDelete(paymentId).then(() => {
+            return res.status(200).json("Payment Deleted");
+        });
+     }catch (error){
+        return res.status(400).json({ error });
+    }
+
+}
