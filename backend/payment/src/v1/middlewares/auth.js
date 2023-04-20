668084ae9351af4eef74e57a8011b6a104146e46
Supun Kariyawasam <bhasura2105@gmail.com>
2023-04-20 11:12:57 +0530

    worked on authentication for payment apis

---
+++ backend/payment/src/v1/middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require('../config');
+
+function authenticateUser(req, res, next) {
+
+    const token = req.header('Authorization');
+
+    if (!token) {
+      return res.status(401).json({ error: 'Authorization token not found.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token.split(' ')[1], config.jwtSecret);
+        req.user = decoded.id;
+        next();
+    } catch (err) {
+        res.status(401).json({ error: 'Invalid authorization token.' });
+    }
+
+  }
+
+module.exports = {
+    authenticateUser,
+};
