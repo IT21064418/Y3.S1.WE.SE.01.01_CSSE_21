e9d6337ea1f564129cbd354b56dc92ff2664987e
Supun Kariyawasam <bhasura2105@gmail.com>
2023-04-24 09:29:40 +0530

    payment service adjustments

--- backend/payment/src/v1/config.js
+++ backend/payment/src/v1/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI: 'mongodb+srv://it21064418:it21064418@dscluster.qczuvg7.mongodb.net/DS_Project?retryWrites=true&w=majority',
+    MONGODB_URI: process.env.MONGO_URI,
     PORT: process.env.PORT || 8003,
     jwtSecret: process.env.JWT_SECRET
 };
