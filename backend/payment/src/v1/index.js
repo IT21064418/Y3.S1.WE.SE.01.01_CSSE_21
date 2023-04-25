cf880500f4c1d06f2993a94ef52f1efe48a65a17
Supun Kariyawasam <bhasura2105@gmail.com>
2023-04-25 12:25:55 +0530

    changes to router and controller of payment service

--- backend/payment/src/v1/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-module.exports.publishDeliveryEvent = async(payload) => {
-
-    axios.post('http://localhost:8000/delivery/app-events', {
-        payload
-    });
-
-}
